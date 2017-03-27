81e7b3c89f96b3eba592ffdf863df6ab5f4f8192
Hlynur Stefánsson <hlynurs15@ru.is>
2017-03-27 22:26:56 +0000

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -228,7 +228,7 @@ class Foreground {
 			this.el1.css('background-size', '100%');
 			this.el2.css('background', 'url(' + this.foregrounds[1] + ') no-repeat');
 			this.el2.css('background-size', '100%');
-			this.speed = 14;
+			this.speed = 12;
 		}
 		else {
 			this.el1.css('background', 'url(' + this.foregrounds[0] + ') no-repeat');
@@ -316,7 +316,7 @@ class Pipes {
 			this.north.css('background-size', '100%');
 			this.south.css('background', 'url(' + this.pipes[1][1] + ') no-repeat');
 			this.south.css('background-size', '100%');
-			this.speed = 14;
+			this.speed = 12;
 		}
 		else {
 			this.north.css('background', 'url(' + this.pipes[0][0] + ') no-repeat');
@@ -410,8 +410,11 @@ class Scoreboard {
 			this.gameOverEl.find('#HighScoreHundred')
 		]
 		this.newLabelEl = this.gameOverEl.find('.newLabel');
+		this.medal = this.gameOverEl.find('.Medal');
 		this.game = game;
 		this.topScore = 0;
+		this.regularTopScore = 0;
+		this.nightmareTopScore = 0;
 		this.currentScore = 0;
 	}
 
@@ -444,7 +447,16 @@ class Scoreboard {
 
 	showGameOverScore () {
 		this.hideLiveScore();
-		
+		this.showMedal();
+
+		if (this.game.nightmareMode) {
+			this.topScore = this.nightmareTopScore;
+			this.nightmareTopScore = (this.currentScore > this.nightmareTopScore) ? this.currentScore : this.nightmareTopScore;
+		} else {
+			this.topScore = this.regularTopScore;
+			this.regularTopScore = (this.currentScore > this.regularTopScore) ? this.currentScore : this.regularTopScore;
+		}
+
 		if (this.currentScore > this.topScore) {
 			this.topScore = this.currentScore;
 			this.newLabelEl.css('background', 'url(./images/new.png) no-repeat');
@@ -471,6 +483,37 @@ class Scoreboard {
 			if(this.HighScoreEl[i].is(':hidden')) {
 				this.HighScoreEl[i].show();
 			}
+		}
+	}
+
+	showMedal() {
+		if (this.currentScore < 10) {
+			this.medal.hide();
+		}
+		else if (this.currentScore >= 10 && this.currentScore <= 19) {
+			this.medal.show();
+			console.log('showing bronse medal');
+			console.log(this.medal);
+			this.medal.css('background', 'url(../images/bronse.png) no-repeat');
+			this.medal.css('background-size', '100%');
+		}
+		else if (this.currentScore >= 20 && this.currentScore <= 29) {
+			this.medal.show();
+			console.log('showing silver medal');
+			this.medal.css('background', 'url(../images/silver.png) no-repeat');
+			this.medal.css('background-size', '100%');
+		}
+		else if (this.currentScore >= 30 && this.currentScore <= 39) {
+			this.medal.show();
+			console.log('showing gold medal');
+			this.medal.css('background', 'url(./images/gold.png) no-repeat');
+			this.medal.css('background-size', '100%');
+		}
+		else if (this.currentScore >= 40 && this.currentScore <= 49) {
+			this.medal.show();
+			console.log('showing platinum medal');
+			this.medal.css('background', 'url(./images/platinum.png) no-repeat');
+			this.medal.css('background-size', '100%');
 		}
 	}
 
@@ -753,7 +796,7 @@ class Player {
 	 */
 	reset () {
 		if (this.game.nightmareMode) {
-			this.jumpSpeed = 0.35;
+			this.jumpSpeed = 0.37;
 		}
 		else {
 			this.jumpSpeed = 0.52;
