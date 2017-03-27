298f9445f181a7e216806810eac9448f4a60e396
Hlynur Stefánsson <hlynurs15@ru.is>
2017-03-27 15:57:23 +0000

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1011,7 +1011,7 @@ class Game_Sounds {
 			this.audiochannels[a]['channel'] = new Audio();				// expected end time for this channel
 		}
 		this.mute = false;
-		this.nightmareTheme = new Audio('/sounds/nightmare_theme.mp3');
+		this.nightmareTheme = new Audio('./sounds/nightmare_theme.mp3');
 		this.playingTheme = false;
 
 		this.muteAudioListener();
